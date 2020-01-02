59d7d5cc0c9f8e01fe5f91b20fa8be699440e34f
LRJliurj <908601417@qq.com>
2020-01-02 19:12:07 +0800

    yolov3 train

--- model_train/keras_yolo3/service/train_service.py
+++ model_train/keras_yolo3/service/train_service.py
@@ -39,7 +39,7 @@ def train_service(group_id, model_id, type, jpg_path, xml_path, classnames, onli
         # 选择最优的mAp 的参数配置保存数据库
         save_train_table(group_id, model_id, type,train_los_time = int(end_time-start_time),val_los_time = int(end_time1-end_time),good_config_params= good_config_params,all_config_params = all_config_params,status = 0,des_msg='')
     except Exception as e:
-        des_msg = str("error:"+e)
+        des_msg = "error:e={}".format(e)
         save_train_table(group_id, model_id, type,
                          train_los_time=0, val_los_time=0,
                          good_config_params='', all_config_params='', status=0,
