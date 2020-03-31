6372a525ed29c8dd01228c01985640a83eef93e7
LRJliurj <908601417@qq.com>
2020-03-31 10:28:24 +0800

    增量增类初始

--- freezers/views.py
+++ freezers/views.py
@@ -190,17 +190,16 @@ class AddTrain(APIView):
         try:
             group_id = int(request.query_params['groupid'])
             model_id = int(request.query_params['modelid'])
-
+            type = int(request.query_params['type'])
             data = request.data
             TrainRecord.objects.create(
                 group_id=group_id,
                 model_id=model_id,
                 upcs=json.dumps(data["upcs"]),
                 datas=json.dumps(data["files"]),
+                type = type,
                 status=0
             )
-
-
             return Response(status=status.HTTP_200_OK)
 
         except Exception as e:
