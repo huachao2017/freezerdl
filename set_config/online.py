b14aafd5147f63a104b62d5b1793dddbe8d1c787
LRJliurj <908601417@qq.com>
2020-04-02 14:12:08 +0800

    dd

--- set_config/online.py
+++ set_config/online.py
@@ -83,7 +83,7 @@ yolov3_predict_params = {
 
 app_config = {
     "online_model_dir": "/data/model/online",
-    "backend_dns": "http://admin.bigmeddata.com"
+    "backend_dns": "http://admin.magexiot.com"
 }
 
 ai_config = {
