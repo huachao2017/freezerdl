5e1cff4ae5c41fac70f83e2a457b4975a1739670
LRJliurj <908601417@qq.com>
2020-06-22 15:34:41 +0800

    dd

--- model_train/keras_yolo3/train_test/train.py
+++ model_train/keras_yolo3/train_test/train.py
@@ -114,4 +114,4 @@ if __name__ == '__main__':
     batch_id = 39
     write_img_name.generate_main_txt(shop_id, batch_id)
     voc_anotion.convert(shop_id, batch_id, class_names=class_names)
-    _main([1],shop_id,batch_id,type=0,online_batch_id=1)
+    _main([1,2,3,4,5],shop_id,batch_id,type=0,online_batch_id=1)
